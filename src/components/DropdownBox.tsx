8aa0de803a0e685048e1c70fc4ba43f95db7f29d
Piolo Torrecampo <94138318+piolo-torrecampo@users.noreply.github.com>
2025-02-17 07:35:17 +0800

    search bar added

--- src/components/DropdownBox.tsx
+++ src/components/DropdownBox.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown, Search } from 'lucide-react';
 import { useState, FC } from 'react';
 import { useId } from "react"
 import { Rates } from '../types/CurrencyAPI';
@@ -10,16 +10,17 @@ interface DropdownBoxProps {
   onSelectCurrency?: (currency: string) => void
 }
 
-
 const DropDownBox: FC<DropdownBoxProps> = ({ title, rates, value, onSelectCurrency }) => {
   const id = useId()
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [queryValue, setQueryValue] = useState<string>('')
 
   // toggle dropdown
   const openDropdown = () => {
     setIsOpen(!isOpen)
   }
 
+  // push selected code
   const returnSelectedCurrency = (currencyCode: string) => {
     setIsOpen(false)
     onSelectCurrency?.(currencyCode)
@@ -27,24 +28,29 @@ const DropDownBox: FC<DropdownBoxProps> = ({ title, rates, value, onSelectCurren
 
   // returns currency name
   const getCurrencyName = (currencyCode: string): string | undefined => {
-    if(rates) {
+    if (rates) {
       return rates[currencyCode]?.name;
     }
-   
   };
 
   // returns currency flag
   const getCurrencyFlag = (currencyCode: string): string | undefined => {
-    if(rates) {
+    if (rates) {
       return rates[currencyCode]?.flagUrl;
     }
   };
 
+  // filter currencies 
+  const filteredRates = rates ? Object.entries(rates).filter(([currencyCode, currencyData]) => {
+    return currencyData.name.toLowerCase().includes(queryValue.toLowerCase()) ||
+      currencyCode.toLowerCase().includes(queryValue.toLowerCase());
+  }) : [];
+
   return (
     <div className='relative inline-block lg:w-full md:w-full sm:w-full' id={id}>
-      <button 
+      <button
         className='flex flex-col p-3 shadow-lg border-2 border-gray-200 shadow-cyan-500/40 bg-white rounded-lg w-60 md:w-full sm:w-full h-21 hover:bg-stone-300'
-        onClick={openDropdown}  
+        onClick={openDropdown}
       >
         <p className='font-(poppins) text-gray-400 text-start'>{title}</p>
         <div className='flex flex-row justify-between items-center w-full'>
@@ -61,30 +67,43 @@ const DropDownBox: FC<DropdownBoxProps> = ({ title, rates, value, onSelectCurren
           />
         </div>
       </button>
-      {isOpen && 
+      {isOpen &&
         <div className="absolute p-1 right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 focus:outline-hidden">
+          <div className='flex flex-row justify-between items-center p-2 border-b border-gray-200'>
+            <div className='flex flex-row justify-center items-center gap-2 w-full'>
+              <Search
+                color='black'
+                className='w-5 h-5'
+              />
+              <input
+                className='font-(poppins) outline-none text-black w-full bg-transparent h-10'
+                placeholder='Search currency...'
+                value={queryValue}
+                onChange={(e) => setQueryValue(e.target.value)}
+              />
+            </div>
+          </div>
+          {/* Dropdown Options */}
           <div className="py-1 max-h-70 overflow-y-auto">
-            {rates && 
-              Object.entries(rates).map(([currencyCode, currencyData]) => (
-                <button 
-                  className="flex flex-row gap-5 items-center justify-between block px-4 text-sm text-gray-700 w-full text-left hover:bg-stone-300"
-                  onClick={() => returnSelectedCurrency(currencyCode)}
-                  key={currencyCode}
-                >
-                  <div className='flex flex-row items-center gap-5'>
-                    <img
-                      src={currencyData.flagUrl}
-                      className='w-10 h-10'
-                      alt={currencyData.name}
-                    />
-                    {currencyData.name}  
-                  </div>                 
-                  <div>
-                    {currencyCode}
-                  </div>
-                </button>
-              ))
-            }
+            {filteredRates.map(([currencyCode, currencyData]) => (
+              <button
+                className="flex flex-row gap-5 items-center justify-between block px-4 text-sm text-gray-700 w-full text-left hover:bg-stone-300"
+                onClick={() => returnSelectedCurrency(currencyCode)}
+                key={currencyCode}
+              >
+                <div className='flex flex-row items-center gap-5'>
+                  <img
+                    src={currencyData.flagUrl}
+                    className='w-10 h-10'
+                    alt={currencyData.name}
+                  />
+                  {currencyData.name}
+                </div>
+                <div>
+                  {currencyCode}
+                </div>
+              </button>
+            ))}
           </div>
         </div>
       }
